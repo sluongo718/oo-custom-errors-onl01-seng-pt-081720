c6d2b49b53187a595e28c0e3f9f1f34eaf62852c
sam l <sluongo718@yahoo.com>
2020-09-20 17:07:41 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,9 +5,13 @@ class Person
     @name = name
   end
 
-  def get_married(person)
+ def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
   
   class PartnerError < StandardError
